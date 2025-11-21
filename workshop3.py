63f224e549fccffaad331b046106eccf348649e6
ananya0511sarkar-prog <ananya0511sarkar@gmail.com>
2025-11-21 14:05:51 +0600

    Add files via upload

---
+++ workshop3.py
@@ -0,0 +1,13 @@
+#Code Example:
+#Analog input
+from machine import Pin, ADC
+import time
+
+pot = ADC(Pin(26))
+conversion_factor = 3.3 / 65535
+
+while True:
+    #pot_voltage = pot.read_u16()
+    pot_voltage = pot.read_u16()*conversion_factor
+    print(pot_voltage)
+    time.sleep(0.1)
